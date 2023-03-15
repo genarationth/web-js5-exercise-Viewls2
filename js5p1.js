b7da0f481541c342f1255bd05edaeb0f39cfb0c8
Viewls2 <mahasamut.kh@gmail.com>
2023-03-15 23:22:36 +0700

    add js5p1 & p2

---
+++ js5p1.js
@@ -0,0 +1,8 @@
+let nameList = ['Sofia','David','Juan']; //part 1
+console.log(nameList);
+nameList.push('Sara','Augustin');
+nameList.unshift('teller'); //part 2
+console.log(nameList);
+nameList.splice(3,0,'Reneta');
+nameList.push('Elena');  // part 3
+console.log(nameList);
---
+++ js5p2.js
@@ -0,0 +1,10 @@
+for (let i = 0; i <5; i++) {
+    let stars= '';
+    for (let j=0; j < i+1; j++) { 
+        stars += '* '        
+    }  
+    console.log(stars);
+}
+let stars = [];
+
+
