710ebc7989c1fb7ecc64e8f71bd5d26a77eabca2
Viewls2 <mahasamut.kh@gmail.com>
2023-03-16 09:59:03 +0700

    add: part 3 homework

--- js5p3.js
+++ js5p3.js
@@ -6,3 +6,36 @@ while (value > 0) {
         value -=0.5;
     }
 }
+
+// 3.2 odd numbers
+const oddNumber = (n) => {
+    let count = 1;
+    while (count <= n) {
+      if (count%2 !== 0) { // false
+        console.log(count);
+      }
+      count++
+    }
+  }
+  oddNumber(100);
+
+// 3.3 loop print number
+const printBracket = (n) => {
+    let count = 1;
+    while (count <= n) {
+      console.log(`[${count}]`);
+      count++;
+    }
+  }
+  printBracket(6);
+// 3.4 sum 1+...+n;
+const sum = (x) => {
+    let i = 0;          // iterations
+    let nums = 0;       // sum result
+    while (i <= x) {
+        nums += i;
+        i++;            // stop parameters
+    }
+    console.log(nums);
+}
+sum(100);
